56262021854715a0e3d6ee7d420bf0e2d1a6e00b
guiworm <dylan.mccrindle@gmail.com>
2016-11-27 20:43:53 -0500

    Initial Commit

---
+++ Eventable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Eventable
+//
+//  Created by Dylan McCrindle on 2016-11-27.
+//  Copyright © 2016 Dylan-Shahab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Eventable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Eventable
+//
+//  Created by Dylan McCrindle on 2016-11-27.
+//  Copyright © 2016 Dylan-Shahab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
